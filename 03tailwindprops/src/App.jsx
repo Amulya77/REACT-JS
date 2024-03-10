50462a6ca9e4fe3fcb60dcc32d9a76a40acdd2b9
Amulya Kumar Maurya <akmewtriple7@gmail.com>
2024-03-11 03:06:02 +0530

    03tailwindprops-updated and used objects

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -3,12 +3,26 @@ import { useState } from 'react'
 // import viteLogo from '/vite.svg'
 import './App.css'
 
+import Card from './components/Card'
+
 function App() {
   const [count, setCount] = useState(0)
 
+  let myobj={
+    username:"amulya",
+    age:22
+  }
+
+  // let newArr=[1,2,3,4]; it can also be passed
+
   return (
     <>
-      <h1 className='bg-green-400 text-black p-4 rounded-xl'>Hello World!</h1>
+      <h1 className='bg-red-400 text-black p-4 rounded-xl mb-4'>Hello World!</h1>
+      <Card username="HelloOne" btnText="Touch me!"  someObj={myobj}/>
+      <Card username="Amulya" btnText="Click me!" />
+      <Card username="Amulya" />
+
+
     </>
   )
 }
