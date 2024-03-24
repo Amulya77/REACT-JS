b5458d806d09a91f8241683d51feb7ac9284bf01
Amulya Kumar Maurya <akmewtriple7@gmail.com>
2024-03-24 22:59:24 +0530

    Updated todo-list/page.js

--- SCS/todo-list/app/page.js
+++ SCS/todo-list/app/page.js
@@ -3,18 +3,51 @@ import React, { useState } from 'react'
 
 const page = () => {
   const[todos, setTodos] = useState("")
+  const[description, setDescription] = useState("")
+
+  const[Maintask  , setMaintask] = useState([])
+
+  const SubmitHandler = (e) => {
+    e.preventDefault()
+    console.log(todos)
+    console.log(description)
+    setTodos("")
+    setDescription("")
+   
+ }
+ let renderTask=<h2>No Task</h2>
+
   return (
     <>
     <h1 className='bg-black p-5 text-5xl text-white text-center'>Todo List</h1>
 
 
-    <form className='flex justify-center items-center'>
+    <form className='flex justify-center items-center' onSubmit={SubmitHandler}>
       <input type='text' className='w-1/3 p-2 m-2 border-zinc-800 border-2 rounded'
-      placeholder='Add ToDo...'/>
+      placeholder='Add ToDo...'
+      value={todos}
+      onChange={(e) => setTodos(e.target.value)}
+      
+      />
       <input type='text' className='w-1/3 p-2 m-2 border-zinc-800 border-2 rounded'
-      placeholder='Enter Description here...'/>
+      placeholder='Enter Description here...'
+      value={description}
+      onChange={(e) => setDescription(e.target.value)}/>
       <button className="p-2 bg-black text-white rounded">Add</button>
     </form>
+
+    <hr/>
+
+    <div className='flex justify-center items-center bg-slate-200 p-6'>  
+      <ul>
+        {renderTask}
+      </ul>
+    
+    </div>
+
+
+
+
     </>
   )
 }
